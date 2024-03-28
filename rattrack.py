a2eaa5f3f0e50d0c48cba912b9e0c3e069bffc3a
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-28 15:33:29 -0700

    Add files via upload

---
+++ rattrack.py
@@ -0,0 +1,9 @@
+import __future__
+import tarfile
+import traceback
+import enum
+try:
+    __future__.barry_as_FLUFL(set.symmetric_difference)
+    traceback.clear_frames(tarfile.AbsolutePathError)
+except:
+    enum.Flag._missing_
